da5ce517fda0b9f32130f240adeeb2d5fa9f0ddb
chpf <chpfgit@gmail.com>
2024-08-23 22:45:34 +0200

    move colors to table

--- colors/mine.lua
+++ colors/mine.lua
@@ -1,1 +1,2 @@
-require("mine").set_highlight()
+require("mine").setup()
+
--- lua/mine/init.lua
+++ lua/mine/init.lua
@@ -1,53 +1,56 @@
 local M = {}
 
 local colors = {
-    highlight = '#FAFF00',
-    background = '#2D2A2E',
-    text = '#FCFCFA',
-    comment = '#CCCCAC',
-    none = 'NONE',
+	highlight = "#FAFF00",
+	background = "#2D2A2E",
+	text = "#FCFCFA",
+	comment = "#CCCCAC",
+	none = "NONE",
 }
 
-M.setup = function()
-
+function M.setup()
+	M.set_highlights()
 end
 
-M.set_highlights = function()
-    vim.cmd("hi clear")
-    if vim.fn.exists("syntax_on") then
-        vim.cmd("syntax reset")
-    end
-    vim.o.background = "dark"
-    vim.o.termguicolors = true
-    vim.g.colors_name = "mine"
+function M.set_highlights()
+	vim.cmd("hi clear")
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
+	end
+	vim.o.background = "dark"
+	vim.o.termguicolors = true
+	vim.g.colors_name = "mine"
 
+	local highlight_groups = {
+		Constant = { bg = colors.background, fg = colors.highlight },
+		Normal = { bg = colors.background, fg = colors.text },
+		Identifier = { bg = colors.background, fg = colors.text },
+		Statement = { bg = colors.background, fg = colors.text },
+		Type = { bg = colors.background, fg = colors.text },
+		Special = { bg = colors.background, fg = colors.text },
+		EndOfBuffer = { bg = colors.background, fg = colors.text },
+		SpecialKey = { bg = colors.background, fg = colors.text },
+		NonText = { bg = colors.background, fg = colors.text },
+		Ignore = { bg = colors.background, fg = colors.text },
+		Comment = { bg = colors.background, fg = colors.comment },
+		Pmenu = { bg = "#1E1E1E", fg = colors.text },
+		PmenuSel = { bg = "#131313", fg = colors.highlight },
+		Cursor = { bg = colors.text, fg = colors.background },
+		Visual = { bg = colors.text, fg = colors.background },
+		Error = { bg = colors.background, fg = "#FF1029" },
+		Todo = { bg = colors.background, fg = "#FFAA00" },
+		fzf1 = { bg = colors.background, fg = colors.text },
+		fzf2 = { bg = colors.text, fg = "#FFAA00" },
+		fzf3 = { bg = colors.background, fg = colors.text },
+		Folded = { bg = colors.background, fg = colors.text },
+		FoldColumn = { bg = colors.background, fg = colors.text },
+		IndentBlanklineIndent = { bg = colors.background, fg = "#5F5F5F" },
+		MatchParen = { bg = colors.background, fg = colors.highlight },
+	}
 
-    vim.api.nvim_set_hl(0, "Constant",              { bg = colors.background, fg = colors.highlight })
-    vim.api.nvim_set_hl(0, "Normal",                { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Identifier",            { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Statement",             { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Type",                  { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Special",               { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "EndOfBuffer",           { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "SpecialKey",            { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "NonText",               { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Ignore",                { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Comment",               { bg = colors.background, fg = colors.comment })
-    vim.api.nvim_set_hl(0, "Pmenu",                 { bg = "#1E1E1E",         fg = colors.text })
-    vim.api.nvim_set_hl(0, "PmenuSel",              { bg = "#131313",         fg = colors.highlight })
-    vim.api.nvim_set_hl(0, "Cursor",                { bg = colors.text,       fg = colors.background })
-    vim.api.nvim_set_hl(0, "Visual",                { bg = colors.text,       fg = colors.background })
-    vim.api.nvim_set_hl(0, "Error",                 { bg = colors.background, fg = "#FF1029" })
-    vim.api.nvim_set_hl(0, "Todo",                  { bg = colors.background, fg = "#FFAA00" })
-    vim.api.nvim_set_hl(0, "fzf1",                  { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "fzf2",                  { bg = colors.text, fg = "#FFAA00" })
-    vim.api.nvim_set_hl(0, "fzf3",                  { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "Folded",                { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "FoldColumn",            { bg = colors.background, fg = colors.text })
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent", { bg = colors.background, fg = '#5F5F5F' })
-    vim.api.nvim_set_hl(0, "MatchParen",            { bg = colors.background, fg = colors.highlight })
+	for group, color in pairs(highlight_groups) do
+		vim.api.nvim_set_hl(0, group, color)
+	end
 end
-
-
 
 return M
